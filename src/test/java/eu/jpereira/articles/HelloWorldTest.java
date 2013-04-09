d61c5fba743c423e9ae00c336bed09333ade0403
jpereira <joaomiguel.pereira@gmail.com>
2013-04-09 21:26:46 +0200

    As usual, the initial commit

---
+++ src/test/java/eu/jpereira/articles/HelloWorldTest.java
@@ -0,0 +1,12 @@
+package eu.jpereira.articles;
+
+import org.testng.annotations.Test;
+
+public class HelloWorldTest
+{
+   @Test
+   public void testGetText() {
+      HelloWorld fixture = new HelloWorld();
+      assert "Hello World!".equals(fixture.getText());
+   }
+}
